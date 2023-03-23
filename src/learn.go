a85f2d9d8bd1fb04696cf506f7359109b835321c
ernestlipson <ernestlipson@amalitech>
2023-03-23 09:35:28 +0000

    learn:multiple variables block

--- src/learn.go
+++ src/learn.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+var (
+	actorNmae string = "Elizabeth"
+	companion string = "Smith"
+	season int = 12
+)
+
 func main() {
-	fmt.Println("Hello, excited with go");
+	var declare int = 78;
+	newDeclare := 99
+	fmt.Println("Hello, excited with go")
+	fmt.Print("Am runiing -------")
+	fmt.Println(declare, newDeclare)
 }
