b5f41379a49a1a3e59561b3b53227aff3bf892fa
ernestlipson <ernestlipson>
2025-03-16 06:52:39 +0000

    refactor investment calculation to include future real value with annual rate increase

--- src/investment_calc/investment.go
+++ src/investment_calc/investment.go
@@ -6,10 +6,20 @@ import (
 )
 
 func main() {
-	var investmentAmt = 1000
-	var interestRate = 0.05
-	var years = 5
+	investment := 1000.0
+	rate := 0.05
+	years := 5.0
 
-	var futureValue = float64(investmentAmt) * math.Pow(1+interestRate, float64(years))
-	fmt.Println("Future value of investment:", futureValue)
+	future := investment * math.Pow(1+rate, years)
+
+	// Calculate future real value with 1% rate increase per year
+	futureRealValue := investment
+	currentRate := rate
+	for i := 0; i < int(years); i++ {
+		futureRealValue *= (1 + currentRate)
+		currentRate += 0.01 // Increase rate by 1%
+	}
+
+	fmt.Println("Future value of investment:", future)
+	fmt.Println("Future real value (with 1% annual rate increase):", futureRealValue)
 }
